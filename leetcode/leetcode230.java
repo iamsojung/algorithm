23ccea3d506c94068ba43373820b101fbdc96ad1
iamsojung <38772161+iamsojung@users.noreply.github.com>
2021-03-08 23:28:53 +0900

    Update leetcode230.java

--- leetcode/leetcode230.java
+++ leetcode/leetcode230.java
@@ -1,1 +1,39 @@
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    int count=0;
+    int ans=0;
+    
+    public int kthSmallest(TreeNode root, int k) {
+        inorder(root,k);
+        return ans;
+    }
+    
+    private void inorder(TreeNode root,int k){
+        
+        if(root==null) return;
+        
+        inorder(root.left,k);
+        count++;
+        
+        if(count==k) {
+            ans=root.val;
+            return;
+        }
+        inorder(root.right,k);
+    }
+}
