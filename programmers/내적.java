d5b5aa53cec4c931bcffd8b24902b7a4cea24bfa
iamsojung <38772161+iamsojung@users.noreply.github.com>
2021-04-26 20:08:47 +0900

    Create 내적.java
    프로그래머스 내적

---
+++ programmers/내적.java
@@ -0,0 +1,13 @@
+class Solution {
+
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for (int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+
+        }
+        answer = answer;
+
+        return answer;
+    }
+}
