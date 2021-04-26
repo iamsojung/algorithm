528f7f7339e34c923fc501b8d1d7ee7e6b24f3de
iamsojung <38772161+iamsojung@users.noreply.github.com>
2021-04-26 22:18:40 +0900

    Create 수박수박수박수박수박수?.java

---
+++ programmers/수박수박수박수박수박수?.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                answer = answer+"수";
+            }else{
+                 answer = answer+"박";
+            }
+            
+        }
+        System.out.println(answer);
+        return answer;
+    }
+}
