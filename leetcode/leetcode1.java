e5d13b8f22c2eed457e7cae4b9efe920f85958be
iamsojung <38772161+iamsojung@users.noreply.github.com>
2021-05-27 21:45:20 +0900

    Update leetcode1.java

--- leetcode/leetcode1.java
+++ leetcode/leetcode1.java
@@ -1,3 +1,24 @@
+/*시간복잡도 O(2N), 공간복잡도 O(N)*/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {        
+
+        Map<Integer,Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            hm.put(nums[i],i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target-nums[i];
+            if(hm.containsKey(diff)&&hm.get(diff)!=i){
+                return new int[]{i,hm.get(diff)};
+            }
+
+        }
+      return null;
+    
+    }
+}
+/*
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
@@ -11,7 +32,6 @@ class Solution {
 
                     answer[0] = i;
                     answer[1] = j;
-
                     
                 }
             }
@@ -19,3 +39,5 @@ class Solution {
         return answer;
     }
 }
+*/
+
