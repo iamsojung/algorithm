70f2fe5ee0fa12037752bccf06dd504482560a9a
iamsojung <38772161+iamsojung@users.noreply.github.com>
2021-07-21 20:40:46 +0900

    최소힙

---
+++ bojboj1927.java
@@ -0,0 +1,33 @@
+package week3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class boj1927 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            int temp = Integer.parseInt(br.readLine());
+            if (temp == 0) {
+                if(pq.size()>0){
+                    System.out.println(pq.poll());
+                }else{
+                    System.out.println(0);
+                }
+
+            }else{
+                pq.add(temp);
+            }
+        }
+
+
+
+    }
+}
