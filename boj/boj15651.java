4745a69aafc57f55e081e96c8a349e4cd64b7e7f
iamsojung <38772161+iamsojung@users.noreply.github.com>
2021-08-07 16:45:38 +0900

    N과 M (3)

--- boj/boj15651.java
+++ boj/boj15651.java
@@ -1,3 +1,5 @@
+package week6;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,45 +7,42 @@ import java.util.StringTokenizer;
 
 public class boj15651 {
 
-	static int N, M;
-	static int[] arr;
-	static boolean[] visited;
-	static StringBuilder sb;
+    static int N, M;
+    static boolean[] visited;
+    static int[] ans;
+    static StringBuilder sb;
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		sb = new StringBuilder();
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
 
-		visited = new boolean[N];
-		arr = new int[M];
+        N = Integer.parseInt(st.nextToken()); //문제의 N의 값 입력받기.
+        M = Integer.parseInt(st.nextToken()); //문제의 M의 값 입력받기.
 
-		func(0);
+        ans = new int[M]; //중복없이 선택한 M개의 숫자를 저장하기 위한 배열
+        visited = new boolean[N + 1]; //중복체크를 해주기 위한 배열
 
-		System.out.println(sb);
+        dfs(0); //탐색을 시작할 숫자 start, 선택한 개수 index로 구분
+        System.out.println(sb.toString());
+    }
 
-	}
+    private static void dfs(int index) {
 
-	static void func(int index) {
+        if (index == M) { //M개가 됐을때 ans에 저장해둔 M개의 숫자 출력.
+            for (int i = 0; i < M; i++) {
+                sb.append(ans[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int j = 1; j <= N; j++) { //중복 상관없다고 했으므로, visited체크 빼줌
+            visited[j] = true;
+            ans[index] = j;
+            dfs(index + 1);
+            visited[j] = false;
+        }
 
-		if (index == M) {
-			for (int i = 0; i < M; i++) {
-				sb.append(arr[i]).append(" ");
-			}
-			sb.append('\n');
-		} else {
-			for (int i = 0; i < N; i++) {
-				visited[i] = true;
-				arr[index] = i + 1;
-				func(index + 1);
-				visited[i] = false;
-			}
-
-		}
-	}
-
+    }
 }
-
