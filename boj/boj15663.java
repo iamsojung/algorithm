cfc8b63c5f2e86467d306e075448dc7e8b298aa1
iamsojung <38772161+iamsojung@users.noreply.github.com>
2021-08-16 01:58:20 +0900

    N과 M(9)

---
+++ boj/boj15663.java
@@ -0,0 +1,74 @@
+package week6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+public class boj15663 {
+
+    static int N, M;
+    static int[] map;
+    static boolean[] visited;
+    static int[] ans;
+    static StringBuilder sb;
+    static HashSet<String> hs = new HashSet<>();
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        sb = new StringBuilder();
+        map = new int[N];
+        ans = new int[M];
+        visited = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            map[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(map);
+        dfs(0);
+        System.out.println(sb.toString());
+    }
+
+    private static void dfs(int index) {
+
+        if (index == M) {
+            String temp = "";
+            for (int i = 0; i < M; i++) {
+                temp = temp + ans[i];
+            }
+            //System.out.println("temp: " + temp);
+
+            if (!hs.contains(temp)) {
+                hs.add(temp);
+                //System.out.println("temp: " + temp);
+                for (int j = 0; j < M; j++) {
+                    sb.append(ans[j] + " ");
+                }
+                sb.append("\n");
+            }
+
+            return;
+
+        }
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                ans[index] = map[i];
+                dfs(index + 1);
+                visited[i] = false;
+            }
+
+        }
+    }
+}
