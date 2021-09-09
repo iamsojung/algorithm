78095178e7af8d26c52f580f9077c1098a2959b5
iamsojung <38772161+iamsojung@users.noreply.github.com>
2021-09-09 18:45:04 +0900

    랜선자르기

--- boj/boj1654.java
+++ boj/boj1654.java
@@ -1,3 +1,5 @@
+package week10;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,38 +7,49 @@ import java.util.StringTokenizer;
 
 public class boj1654 {
 
-  public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-       int K = Integer.parseInt(st.nextToken());
-       int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
-       long[] arr = new long[K];
-       long max=0;
+        //K를 N개로 만들어야함
+        long[] arr = new long[K];
+        long max = 0;
+        for (int i = 0; i < K; i++) {
 
-       for(int i=0;i<K;i++){
-           arr[i]=Long.parseLong(br.readLine());
-           max = Math.max(max,arr[i]);
+            arr[i] = Long.parseLong(br.readLine());
+            max = Math.max(max, arr[i]);
 
-       }
-       //이분탐색
-       long left = 1;
-       long right = max;
-       while(left<=right){
-           long mid = (left+right)/2;
-           long sum = 0;
-           for(int i=0;i<K;i++){
-               sum+=(arr[i]/mid);
-           }
-           if(sum>=N){
-               left = mid+1;
-           }else{
-               right = mid-1;
-           }
-       }
-       System.out.println(right);
-   }
+        }
+
+        long start = 1;
+        long end = max;
+        long mid = 0;
+
+        while (start <= end) {
+
+            mid = (start + end) / 2;
+
+            long count = 0;
+
+            for (int i = 0; i < K; i++) {
+
+                long temp = arr[i] / mid;
+                count = count + temp;
+
+            }
+            if (count >= N) {
+                start = mid + 1;
+            }
+            if (count < N) {
+                end = mid - 1;
+            }
+        }
+        System.out.println(start-1);
+
+
+    }
 }
-
