428dd09ad1f242975d7d31a97828d844eb2989e3
iamsojung <38772161+iamsojung@users.noreply.github.com>
2022-01-16 00:45:53 +0900

    Create leetcode121.java

---
+++ leetcode/leetcode121.java
@@ -0,0 +1,17 @@
+class Solution {
+   public int maxProfit(int[] prices) {
+    int max =0;
+    int min = Integer.MAX_VALUE;
+    
+    for(int i =0; i<prices.length;i++){
+        if(min > prices[i]){
+            min = prices[i];
+        }
+        else{
+            max = Math.max(max, prices[i]-min);
+        }
+        
+    }
+    return max;
+    }
+}
