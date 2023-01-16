5c51d3ad922706eebd7cd2c5347cd1277dd00584
iamsojung <38772161+iamsojung@users.noreply.github.com>
2023-01-16 22:03:57 +0900

    Create 같은숫자는싫어.java

---
+++ programmers/같은숫자는싫어.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        ArrayList<Integer> tempList = new ArrayList<Integer>();
+        int preNum = 10;
+        for(int num : arr) {
+            if(preNum != num)
+                tempList.add(num);
+            preNum = num;
+        }       
+        int[] answer = new int[tempList.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = tempList.get(i).intValue();
+        }
+        return answer;
+    }
+}
