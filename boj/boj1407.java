c275f9abbfdd7161a1433e1344f011a640c7407e
iamsojung <38772161+iamsojung@users.noreply.github.com>
2023-02-05 20:53:05 +0900

    Create boj1407.java

---
+++ boj/boj1407.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        long A = Long.parseLong(input[0]);
+        long B = Long.parseLong(input[1]);
+
+        System.out.println(function(B)-function(A-1));
+    }
+
+    static long function(long x) {
+        long sum = 0;
+        long y;
+        long i = 1;
+
+        while(x>0) {
+            if(x%2==0)
+                y = x/2;  
+            else
+                y = x/2+1; //
+
+            sum += y*i;
+            x -= y;
+            i*=2;
+        }
+
+        return sum;
+    }
+}
